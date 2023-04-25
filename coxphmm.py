119a37e18625f17749fbaf7a8d1d4ff62c18d43a
Kodi Taraszka <ktaraszka@Kodis-MacBook-Pro.local>
2023-04-25 13:55:35 -0500

    working method but many limitations

---
+++ coxphmm.py
@@ -0,0 +1,161 @@
+'''
+Author: Kodi Taraszka
+Email: Kodi_Taraszka@dfci.harvard.edu
+This is prelim code for cox proportional hazard mulitple mixed model components (CoxPHMM)
+'''
+
+#TODO: double check that left censoring and right censoring work correctly
+#TODO: implement Breslow tie breaker, now we add noise to them :(
+#TODO: classier handling of output/write to file
+#TODO: estimate standard error of tau
+#TODO: offer a correction to the estimated tau
+
+import numpy as np
+import pandas as pd
+import pybobyqa
+from input import IO
+
+class COXPHMM(IO):
+
+	def __init__(self):
+		super().__init__()
+		#From IO, we have N, M, grm, times, events, fixed, output, (but not these two for now) method, solver
+		self.beta = np.zeros(self.M)	
+		self.u = np.zeros(self.N)
+		self.exp_eta = np.zeros(self.N)
+		self.risk_set = np.tril(np.ones((self.N, self.N))).T
+		self.risk_eta = np.zeros((self.N,self.N))
+		self.grm_u_tau = np.zeros(self.N)
+		self.loc = np.where(self.events==1)
+		self.ONE = np.ones(self.N)
+		self.MTW = np.zeros((self.N, self.N))
+		self.WB = np.zeros(self.N)
+		self.A = np.identity(self.N)
+		self.B = np.zeros(self.N)
+		self.H = np.identity(self.N)
+		self.s = np.zeros((self.M+self.N))
+		self.V = np.zeros((self.M+self.N, self.M+self.N))
+		self.theta = np.zeros((self.M+self.N))	
+		# Eigen decompositon & set 0 eigenvalues to 1e-6
+		Lambda, U = np.linalg.eigh(self.grm)#, driver = 'evd')
+		Lambda[Lambda < 1e-10] = 1e-6
+		# grm is now inverted
+		self.grm = np.matmul(np.matmul(U, np.diag(1/Lambda)), np.transpose(U))
+		tau = 0.5
+		soln = pybobyqa.solve(self.marg_loglike, x0 = [tau], bounds = ([1e-4], [5]))
+		print(soln)
+
+	# for each non-censored person, who is at risk at that time
+	def R_j(self):
+		# TODO check edge cases
+		if np.unique(self.times[:,0]).shape[0] > 1:
+			min_start = np.min(self.times[:,0])
+			late_start = np.where(self.times[:,0]!=min_start)[0]
+			for who in late_start:
+				startTime = self.times[who,0]
+				for before in range(0,who):
+					if startTime <= self.times[before,0]:
+						self.risk_set[0:(before-1),who] = 0
+						break
+		# don't fix right censoring now do it inline
+
+	# l_1 as defined in equations 2 in COXMEG paper (mostly their notation)
+	def l_1(self, tau):
+		# eta = Xb + Zu
+		# works even when no fixed effects, just become 0
+		if self.M > 0:
+			self.exp_eta = np.exp(np.matmul(self.fixed, self.beta) + self.u)
+		else:
+			self.exp_eta = np.exp(self.u)
+		self.risk_eta[:,:] = np.multiply(self.risk_set[self.loc,:][0], self.exp_eta)
+		# NOTE: changing the order of tau division may impact likelihood precision
+		self.grm_u_tau = np.matmul(self.grm, self.u)/tau
+		one = np.sum(np.log(self.exp_eta[self.loc]))
+		two = np.sum(np.log(np.sum(self.risk_eta,axis=1)))
+		three = 0.5*(np.matmul(self.u.T, self.grm_u_tau)) # + self.N*np.log(tau))
+		return (np.sum(np.log(self.exp_eta[self.loc])) - np.sum(np.log(np.sum(self.risk_eta,axis=1))) \
+				 - 0.5*(np.matmul(self.u.T, self.grm_u_tau))) #+ self.N*np.log(tau)))
+	
+
+	# l_2 as defined in Equation 3 in COXMEG paper (mostly their notation)
+	def l_1_deriv(self, tau):
+		# W = diag(exp(eta)) but working with exp(eta) explicitly (self.exp_eta)
+		self.MTW = np.multiply(self.risk_set, self.exp_eta)
+		# A = diag(D) diag^-1(M^TW1)
+		self.A = np.multiply(self.events, 1/np.sum(self.MTW, axis=1))
+		# B = diag(MA1)
+		self.B = np.sum(np.multiply(self.risk_set.T, self.A), axis = 1)
+		# H = WB - QQ^T = WB - WMA^2M^TW
+		# NOTE: WB = WMA1
+		self.WB = np.multiply(self.exp_eta, self.B)
+		self.H = np.diag(self.WB) - np.matmul(np.multiply(np.multiply(self.exp_eta, self.risk_set.T), np.square(self.A)), self.MTW)
+		# setting information matrix V with quadrants [[one, two], [three, four]]
+		#V[four] -- H + sigma^-1/tau: always exists since we're looking at random effect		 
+		self.V[self.M:(self.M+self.N), self.M:(self.M+self.N)] = np.add(self.H, (self.grm/tau))
+		# setting score function s with parts [one, two]
+		# s[two] -- d - WMA1 - (Sigma^-1 gamma) / tau (save sigma^-1 gamma / tau for now)
+		self.s[self.M:(self.M+self.N)] = self.events - self.WB
+		# one, two, three only exist if there were fixed effect/covariates
+		if self.M > 0:
+			#V[two] -- X^TH
+			self.V[0:self.M, self.M:(self.M+self.N)] = np.matmul(self.fixed.T, self.H)
+			#V[one] -- X^THX = (V[two]X)
+			self.V[0:self.M, 0:self.M] = np.matmul(self.V[0:self.M, self.M:(self.M+self.N)], self.fixed)
+			#V[three] -- HX = (X^TH)^T = V[two]^T
+			self.V[self.M:(self.M+self.N), 0:self.M] = self.V[0:self.M, self.M:(self.M+self.N)].T
+			#s[one] -- X^T(d - WMA1)
+			self.s[0:self.M] = np.matmul(self.fixed.T, self.s[self.M:(self.M+self.N)])
+		self.s[self.M:(self.M+self.N)] = self.s[self.M:(self.M+self.N)] - self.grm_u_tau
+
+	# treat l_2 as a marginal log-likelihood to get estimate for tau
+	def marg_loglike(self, tau):	
+		# initialize	
+		eps = 1e-6
+		run = 0
+		max_runs = 200
+		loglike = 0
+		update_loglike = self.l_1(tau)
+		diff_loglike = 0 #update_loglike - loglike
+		eps_s = 0 #eps*(-1)*loglike
+		while (run == 0 or diff_loglike > eps) and run < max_runs:
+			damp = 1
+			loglike = update_loglike
+			self.l_1_deriv(tau)
+			# Now solve either direct or pcg?
+			#if self.solver == 'Cholesky':
+			self.theta = np.linalg.solve(self.V, self.s)
+			#else:
+			#self.theta = self.pcg(self.V, self.s, 1e-6)			
+			self.u = self.u + self.theta[self.M:(self.M+self.N)]	
+			if self.M > 0:
+				self.beta = self.beta + self.theta[0:self.M]
+
+			update_loglike = self.l_1(tau)
+			# TODO: add comments to describe
+			eps_s = eps*(-1)*loglike
+			diff_loglike = update_loglike - loglike
+			# TODO: copy-paste, but should actually know why
+			while diff_loglike < -eps_s:
+				damp = damp/2
+				if damp < 1e-2:
+					print("The optimization of PPL may not converge.")
+					diff_loglike = 0
+					break
+				self.theta = damp*self.theta
+				self.u = self.u - self.theta[self.M:(self.M+self.N)]
+				if self.M > 0:
+					self.beta = self.beta - self.theta[0:self.M]
+				update_loglike = self.l_1(tau)
+				diff_loglike = update_loglike - loglike
+
+			run += 1
+		
+		if run == max_runs:
+			print("The PPL ran for the maximum number of iterations (" + str(max_runs) + "). It probably didn't converge")
+			
+		_, log_det = np.linalg.slogdet(self.V)
+		return (self.N*np.log(tau) + log_det + (-2)*update_loglike)
+
+
+if __name__=="__main__":
+	COXPHMM()
---
+++ input.py
@@ -0,0 +1,192 @@
+'''
+Author: Kodi Taraszka
+Email: Kodi_Taraszka@dfci.harvard.edu
+This script is the preliminary io script for coxphmm.py
+'''
+
+import numpy as np
+import argparse
+import pandas as pd
+import os
+
+
+# TODO: handle missing data, assumes data preprocessed
+
+# This class handles all input/output functions
+class IO():
+
+	def __init__(self):
+		self.N = 0
+		self.M = 0
+		self.grm = []
+		self.events = None
+		self.fixed = None
+		self.output = ''
+		#self.method = ''
+		#self.solver = 'exact'
+		self.setup()
+		self.times = self.events[['start', 'stop']].to_numpy()
+		self.events = self.events.event.to_numpy()
+		#TODO: allow more than one GRM for analyses
+		# already allow it as input, but only have one
+		self.grm = self.grm[0]
+
+	# this function calls the parser and processes the input
+	def setup(self):
+		args = self.def_parser()
+		self.output = args.output
+		#self.method = args.method
+		self.process_events(args.sample_id, args.events)
+		if args.random is not None:
+			for i in args.random:
+				self.compute_grm(i, args.sample_id)
+				first = False
+
+		elif args.grm is not None:
+			for i in range(0, len(args.grm)):
+				grm = pd.read_csv(args.grm[i], sep = '\t', header = None)
+				if grm.shape[0] != grm.shape[1]:
+					raise ValueError("GRM: " + args.grm[i] + " is not a square matrix")
+
+				grm_names = pd.read_csv(args.grm_names[i], sep = '\t', header = 0)
+				if grm.shape[0] != grm_names.shape[0]:
+					raise ValueError("The GRM and GRM names file are not the same size")
+				
+				if args.sample_id is not None:
+						grm_names = grm_names.rename(columns = {args.sample_id:'sample_id'})
+
+				grm_names = grm_names.sample_id.to_list()
+				grm.index, grm.columns = grm_names, grm_names		
+				if i != 0:
+					compare = np.unique(self.grm[0].sort_index().index == grm.sort_index().index)
+					if len(compare) > 1 or compare[0] == False:
+						raise ValueError("The sample ids are not consistent across GRM files")
+
+				grm = grm.reindex(index = self.events.index, columns = self.events.index)
+				self.grm.append(grm.to_numpy())
+
+		else:
+			raise ValueError("Need to provide GRM or features to create ``GRM``")
+
+		if args.fixed is not None:
+			self.fixed = pd.read_csv(args.fixed, sep = '\t', header = 0)
+			if args.sample_id is not None:
+				self.fixed = self.fixed.rename(columns = {args.sample_id:'sample_id'})
+
+			self.fixed = self.fixed.set_index('sample_id')
+			columns = self.fixed.columns
+			for col in columns:
+				if self.fixed[col].dtype == object:
+					print("Column: " + col + " is a categorical variable and will be one hot encoded")
+					one_hot = pd.get_dummies(self.fixed[col])
+					one_hot.pop(one_hot.columns[0])
+					self.fixed = self.fixed.drop(col, axis = 1)
+					self.fixed = self.fixed.join(one_hot)
+				elif self.fixed[col].dtype == np.float64:	
+					var = self.fixed[col].var()
+					if(var > 100 or var < 0.01):
+						print("Column: " + col + " has a variance > 100 or < 0.01 and will be centered and scaled")
+						mean = self.fixed[col].mean()
+						self.fixed[col] = (self.fixed[col] - mean)/np.sqrt(var)
+						print("mean: " + str(self.fixed[col].mean()) + " var: " + str(self.fixed[col].var()))
+
+			if len(self.grm) > 0:
+				compare = np.unique(self.grm[0].sort_index().index == self.fixed.sort_index().index)
+				if len(compare) > 1 or compare[0] == False:
+						raise ValueError("The sample ids are not consistent across file")
+
+			self.fixed = self.fixed.reindex(index = self.events.index)
+			self.fixed = self.fixed.to_numpy()
+			self.M = self.fixed.shape[1]
+		else:
+			self.fixed = np.array([])
+			self.M = 0
+
+	# this function contains the parser and it's arguments
+	def def_parser(self):
+		parser = argparse.ArgumentParser(description = "This program runs a cox proportional hazard mixed model with multiple components")
+
+		required = parser.add_argument_group('Required Arguments')
+		required.add_argument('-e', '--events', required = True,
+			help = 'path to outcome file which contains four columns: sample_id, start, stop, event with sample_id changeable by argument -s/--sample_id')
+
+		optional = parser.add_argument_group('Optional Arguments (all for now)')
+		optional.add_argument('-s', '--sample_id', dest = 'sample_id',
+			help = 'column name across files to merge outcome, fixed effect and random effect features. Default is sample_id')
+		optional.add_argument('-f', '--fixed', dest = 'fixed',
+			help = 'path tab delim file containing fixed effects features. First row containing column names')
+		optional.add_argument('-r', '--random', dest = 'random', nargs='*',
+			help = 'path of tab delim file(s) containing random effect features. First row of each file contains column names')
+		optional.add_argument('-g', '--grm', dest = 'grm', nargs='*',
+			help = 'path to tab delim files containing precomputed relatedness matrices. There is no header row in any file nor is there a column for sample ids. Only use one of -g/-grm or -r/--random')
+		optional.add_argument('-gn', '--grm_names', dest = 'grm_names', nargs='*',
+			help = 'path to tab delim files(s) containing sample_id (in order) for each grm with a header row. Must provide one for each grm. Only use one of -g/--grm or -r/--random.')
+		#optional.add_argument('-m', '--method', dest = 'method', default = "BOBYQA",
+		#	help = 'solver method passed to nlopt, default is BOBYQA')
+		optional.add_argument('-o', '--output', dest = 'output', default = 'results.txt',
+			help = 'path to output file. Default = results.txt')
+	
+		args = parser.parse_args()
+		if args.fixed is None and args.random is None and args.grm is None:
+			raise ValueError("There is are no fixed or random effects, please include at least one")
+
+		if args.fixed is not None:
+			if not os.path.isfile(args.fixed):
+				raise ValueError("The fixed effect file does not exist")
+
+		if args.random is not None and args.grm is not None:
+			 raise ValueError("Please only use -r/--random or -g/--grm not both")
+
+		if args.random is not None:
+			for i in args.random:
+				if not os.path.isfile(i):
+					raise ValueError("Random effect input file: " + i + " does not exist")
+
+		if args.grm is not None:
+			if args.grm_names is None:
+				raise ValueError("Each GRM needs a matching file containing the sample names in order")
+			if len(args.grm) != len(args.grm_names):
+				raise ValueError("The number of arguments passed to -g/--grm does not equal the number of arguments passed to -gn/--grm_names")
+			
+			for i in range(0, len(args.grm)):
+				if not os.path.isfile(args.grm[i]):
+					raise ValueError("GRM input file: " + args.grm[i] + " does not exist")
+				if not os.path.isfile(args.grm_names[i]):
+					raise ValueError("GRM input file: " + args.grm_names[i] + " does not exist")
+
+		return(args)
+
+	def process_events(self, sample_id, file):
+		self.events = pd.read_csv(file, sep = '\t', header = 0)
+		if self.events.shape[1] != 4:
+			raise ValueError("There should be four columns in the events/outcome file")
+
+		if sample_id is not None:
+			self.events = self.events.rename(columns = {sample_id:'sample_id'})
+		
+		self.N = self.events.shape[0]
+		self.events = self.events[(self.events.stop > self.events.start)]
+		if self.events.shape[0] != self.N:
+			print(str(orig - self.events.shape[0]) + " individuals dropped because start time is after end time")
+			self.N = self.events.shape[0]
+	
+		#TODO implement Breslow
+		if self.events[(self.events.stop.duplicated())].shape[0] > 0:
+			print(str(self.events[(self.events.stop.duplicated())].shape[0]) + " stop times are duplicated, adding random noise with mean 0 and variance 1e-6")
+			print("TODO: implement Breslow method of tie breaking")
+			noise = np.random.normal(0, 1e-6, self.events.stop[(self.events.stop.duplicated())].shape[0])
+			self.events.loc[(self.events.stop.duplicated()), 'stop'] = self.events.stop[(self.events.stop.duplicated())] + noise
+		
+		self.events = self.events.sort_values("stop", ascending=True).reset_index(drop=True)
+		self.events = self.events.set_index('sample_id')
+
+	# this function process the random effect files
+	def compute_grm(self, file, name):
+		random = pd.read_csv(file, sep = '\t', header = 0)
+		if name is not None:
+			random = random.rename(columns = {name:'sample_id'})
+
+		random = random.set_index('sample_id')
+		random = random.reindex(index = self.events.index)
+
+		self.grm.append((random.dot(random.T) / random.shape[1]).to_numpy())
