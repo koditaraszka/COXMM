6cfe006fd715af9b4144f1f84cf9cc6edcbd4616
Kodi Taraszka <koditaraszka@ucla.edu>
2023-04-29 06:34:12 -0500

    tiny changes

--- coxphmm.py
+++ coxphmm.py
@@ -40,37 +40,37 @@ class COXPHMM(IO):
 		if self.M > 0:
 			fixed = np.memmap(path.join(self.temp, self.fixed), dtype='float64', mode='r', shape=(self.N, self.M))
 			exp_eta[:] = np.exp(np.matmul(fixed, theta[0:self.M]) + theta[self.M:(self.M+self.N)])
+			del fixed
 		else:
 			exp_eta[:] = np.exp(theta)
 		
-		risk_set = np.memmap(path.join(self.temp, self.risk_set), dtype='float64', mode='r+', shape=(self.N,self.N))
+		risk_set = np.memmap(path.join(self.temp, self.risk_set), dtype='float64', mode='r', shape=(self.N,self.N))
 		loc = np.memmap(path.join(self.temp, self.loc), dtype='int64', mode='r', shape=(self.uncensored))
 		risk_eta = np.multiply(risk_set[loc,:], exp_eta)
 		del risk_set
- 
+
+		results = np.sum(np.log(exp_eta[loc])) - np.sum(np.log(np.sum(risk_eta,axis=1)))
+		del exp_eta, loc, risk_eta 
+		
 		grm = np.memmap(path.join(self.temp, self.grm), dtype='float64', mode='r', shape=(self.N,self.N))
 		grm_u = np.memmap(path.join(self.temp, self.grm_u), dtype='float64', mode='r+', shape=(self.N))
 		grm_u[:] = np.matmul(grm, theta[self.M:(self.M+self.N)])
 		del grm	
 		
-		result = (np.sum(np.log(exp_eta[loc])) - np.sum(np.log(np.sum(risk_eta,axis=1))) \
-				 - 1/(2*tau)*(np.matmul(theta[self.M:(self.M+self.N)].T, grm_u))) 
-		
-		del exp_eta, theta, grm_u
-		# don't need to del loc or fixed since they don't need flush() called
+		result -= 1/(2*tau)*(np.matmul(theta[self.M:(self.M+self.N)].T, grm_u))
+		del theta, grm_u
 		return result
 
 	# l_2 as defined in Equation 3 in COXself.MEG paper (mostly their notation)
 	def l_1_deriv(self, tau):
-		risk_set = np.memmap(path.join(self.temp, self.risk_set), dtype='float64', mode='r+', shape=(self.N,self.N))
+		risk_set = np.memmap(path.join(self.temp, self.risk_set), dtype='float64', mode='r', shape=(self.N,self.N))
 		exp_eta = np.memmap(path.join(self.temp, self.exp_eta), dtype='float64', mode='r+', shape=(self.N))
 
 		# W = diag(exp(eta)) but working with exp(eta) explicitly (exp_eta)
 		MTW = np.multiply(risk_set, exp_eta)
 		# A = diag(D) diag^-1(self.M^TW1)
 		events = np.memmap(path.join(self.temp, self.events), dtype='int64', mode='r', shape=(self.N))
-		A = np.multiply(events, 1/np.sum(MTW, axis=1))
-		
+		A = np.multiply(events, 1/np.sum(MTW, axis=1))		
 		# NOTE: WB = WMA1 
 		WB = np.multiply(exp_eta, np.sum(np.multiply(risk_set.T, A), axis = 1))
 		# setting score function s with parts [one, two]
@@ -78,9 +78,11 @@ class COXPHMM(IO):
 		# s[two] -- d - WMA1 - (Sigma^-1 gamma) / tau (save sigma^-1 gamma / tau for now)
 		s[self.M:(self.M+self.N)] = events - WB
 		del events
+
 		# H = WB - QQ^T = WB - WMA^2M^TW	
 		H = np.diag(WB) - np.matmul(np.multiply(np.multiply(exp_eta, risk_set.T), np.square(A)), MTW)
 		del risk_set, exp_eta, WB, MTW, A
+
 		# setting information matrix V with quadrants [[one, two], [three, four]]
 		#V[four] -- H + sigma^-1/tau: always exists since we're looking at random effect		 
 		V = np.memmap(path.join(self.temp, self.V), dtype='float64', mode='r+', shape=(self.N+self.M, self.N+self.M))
@@ -101,7 +103,7 @@ class COXPHMM(IO):
 			del fixed
 		del H
 		# wait to do this in case, we need d - WMA1 if self.M > 0
-		grm_u = np.memmap(path.join(self.temp, self.grm_u), dtype='float64', mode='r+', shape=(self.N))
+		grm_u = np.memmap(path.join(self.temp, self.grm_u), dtype='float64', mode='r', shape=(self.N))
 		s[self.M:(self.M+self.N)] = s[self.M:(self.M+self.N)] - grm_u/tau		
 		del V, s, grm_u
 
@@ -119,10 +121,12 @@ class COXPHMM(IO):
 			damp = 1
 			loglike = update_loglike
 			self.l_1_deriv(tau)
-			theta = np.memmap(path.join(self.temp, self.theta), dtype='float64', mode='r+', shape=(self.N+self.M))
-			V = np.memmap(path.join(self.temp, self.V), dtype='float64', mode='r+', shape=(self.N+self.M,self.N+self.M))
-			s = np.memmap(path.join(self.temp, self.s), dtype='float64', mode='r+', shape=(self.N+self.M))
+			V = np.memmap(path.join(self.temp, self.V), dtype='float64', mode='r', shape=(self.N+self.M,self.N+self.M))
+			s = np.memmap(path.join(self.temp, self.s), dtype='float64', mode='r', shape=(self.N+self.M))
 			update = np.linalg.solve(V, s)
+			del V,s
+			theta = np.memmap(path.join(self.temp, self.theta), dtype='float64', mode='r+', shape=(self.N+self.M))
+
 			theta[self.M:(self.M+self.N)] = theta[self.M:(self.M+self.N)] + update[self.M:(self.M+self.N)]
 			if self.M > 0:
 				theta[0:self.M] = theta[0:self.M] + update[0:self.M]
@@ -155,7 +159,7 @@ class COXPHMM(IO):
 		if run == max_runs:
 			print("The PPL ran for the maximum number of iterations (" + str(max_runs) + "). It probably didn't converge")
 			
-		V = np.memmap(path.join(self.temp, self.V), dtype='float64', mode='r+', shape=(self.N+self.M,self.N+self.M))
+		V = np.memmap(path.join(self.temp, self.V), dtype='float64', mode='r', shape=(self.N+self.M,self.N+self.M))
 		_, log_det = np.linalg.slogdet(V)
 		return (self.N*np.log(tau) + log_det + (-2)*update_loglike)
 
